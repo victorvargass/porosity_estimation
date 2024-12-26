3f4acb0a9db24383bcc7b113ec5a00312b915207
Victor Vargas <victorvargassandoval93@gmail.com>
2024-12-26 18:46:34 -0300

    EDIT: set for sound device input signal

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
-from utils import calculate_bins, load_wav_files, calculate_k, calculate_spectrums, calculate_coherence, calculate_transfer_function, calculate_reflection, calculate_absorption, plot_signals, generate_csv
+from utils import calculate_bins, load_wav_files, record_sample, get_sound_devices, calculate_k, calculate_spectrums, calculate_coherence, calculate_transfer_function, calculate_reflection, calculate_absorption, plot_signals, generate_csv
 from pathlib import Path
 import numpy as np
 
 if __name__ == "__main__":
     # Parameters
-    # fs = 48000 # Sample rate
+    fs = 48000 # Sample rate
     N = 8192 # Number of samples
     bins = calculate_bins(N)
     M = 50 # Number of iterations
@@ -18,15 +18,15 @@ if __name__ == "__main__":
     x_min, x_max = 0, 3000
     y_min, y_max = 0, 1
 
-    #sound_devices = get_sound_devices("Scarlett")
+    #sound_devices = get_sound_devices()
     #print(sound_devices)
-    #device_index = 1
+    device_index = 1
 
     samples_folder = Path('samples/')
     channel1_file = samples_folder / 'Take1_Audio 1-1.wav'
     channel2_file = samples_folder / 'Take1_Audio 2-1.wav'
     filename = 'Muestra 1'
-    sample_data, fs = load_wav_files(channel1_file, channel2_file, N) # Cut the sample in N size splits 
+    #sample_data, fs = load_wav_files(channel1_file, channel2_file, N) # Cut the sample in N size splits 
 
     freqs = fs * np.arange(bins) / N
     k = calculate_k(freqs, C)
@@ -35,8 +35,9 @@ if __name__ == "__main__":
 
     for iteration in range(1, M + 1):
         print(f"Iteración {iteration} iniciada")
-        # sample_data = record_sample(N, fs, device_index)
-        S_11, S_22, S_12 = calculate_spectrums(sample_data[iteration-1])
+        sample_data = record_sample(N, fs, device_index)
+        #S_11, S_22, S_12 = calculate_spectrums(sample_data[iteration-1])
+        S_11, S_22, S_12 = calculate_spectrums(sample_data)
 
         S11_sum = S_11 + (iteration-1)*S11_aux
         S22_sum = S_22 + (iteration-1)*S22_aux
