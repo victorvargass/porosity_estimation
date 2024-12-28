de0d34eb705604403516b868139a149318494561
Victor Vargas <victorvargassandoval93@gmail.com>
2024-12-27 21:45:43 -0300

    EDIT: color buttons and design details

--- ui.py
+++ ui.py
@@ -30,11 +30,6 @@ screen_height = screen.height
 viewport_x = (screen_width - 400) // 2
 viewport_y = (screen_height - 800) // 2
 
-ALERT_WIDTH = 350
-ALERT_HEIGHT = 120
-alert_pos_x = (screen_width - ALERT_WIDTH) // 2
-alert_pos_y = (screen_height - ALERT_HEIGHT) // 2 - 300
-
 # -----------------------------
 #  FUNCIONES DE LA INTERFAZ
 # -----------------------------
@@ -164,7 +159,7 @@ def stop_measurement(sender, app_data, user_data):
     # Creamos la ruta dinámica
     folder_path = f'results/{measurement_data["identification"]}/'
     abs_path = str(Path(folder_path).resolve())
-    new_text = f"Archivos guardados con éxito\n\nCarpeta: {folder_path}"
+    new_text = f"Archivos guardados con éxito"
     
     # Actualizamos el texto
     dpg.configure_item("alert_text", default_value=new_text)
@@ -211,10 +206,35 @@ with dpg.theme() as blue_line_theme:
         # Establece el grosor de la línea a 2.0 píxeles
         dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 2.0, category=dpg.mvThemeCat_Plots)
 
+with dpg.theme(tag="green_button_theme"):
+    with dpg.theme_component(dpg.mvButton):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, [102, 187, 106])  # Verde suave
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [76, 175, 80])  # Verde más intenso al pasar el mouse
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [67, 160, 71])  # Verde oscuro al presionar
+
+with dpg.theme(tag="red_button_theme"):
+    with dpg.theme_component(dpg.mvButton):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, [244, 67, 54])  # Rojo suave
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [229, 57, 53])  # Rojo más intenso al pasar el mouse
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [211, 47, 47])  # Rojo oscuro al presionar
+
+with dpg.theme(tag="dark_yellow_button_theme"):
+    with dpg.theme_component(dpg.mvButton):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, [255, 214, 10])  # Amarillo oscuro
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [255, 193, 7])  # Más oscuro al pasar el mouse
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [255, 179, 0])  # Aún más oscuro al presionar
+
+with dpg.theme(tag="blue_button_theme"):
+    with dpg.theme_component(dpg.mvButton):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, [33, 150, 243])  # Azul claro
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [30, 136, 229])  # Azul más intenso al pasar el mouse
+        dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [25, 118, 210])  # Azul aún más oscuro al presionar
+
 # Cargar la fuente
 with dpg.font_registry():
     try:
         default_font = dpg.add_font("Roboto-Regular.ttf", 18)
+        font_large = dpg.add_font("Roboto-Regular.ttf", 20)
         dpg.bind_font(default_font)
     except:
         print("Error al cargar la fuente 'Roboto-Regular.ttf'. Asegúrate de que el archivo existe.")
@@ -224,14 +244,15 @@ with dpg.font_registry():
 #  VENTANA DE CONFIGURACIÓN
 # -----------------------------
 with dpg.window(
-    label="Configuracion",
+    label="Configuración inicial",
     tag="config_window",
     width=400, height=800,
     no_move=True, no_resize=True, no_title_bar=True
 ):
-    dpg.add_text("Configuracion", pos=(150, 40))
-    dpg.add_spacer(height=10)
-    with dpg.group(horizontal=False, pos=(50, 80)):
+    dpg.add_text("Configuración inicial", pos=(125, 40), tag="config_text")
+    dpg.bind_item_font("config_text", font_large)  # Asigna la fuente al texto
+    
+    with dpg.group(horizontal=False, pos=(50, 90)):
         dpg.add_text("Identificacion")
         dpg.add_input_text(tag="identification", default_value="Identificacion 1", width=300)
         dpg.add_text("Localizacion")
@@ -271,9 +292,9 @@ with dpg.window(
                 dpg.add_text("Promediaciones")
                 dpg.add_input_int(
                     tag="averages",
-                    default_value=50,
+                    default_value=200,
                     min_value=0,
-                    max_value=100,
+                    max_value=1000,
                     width=150,
                     step=0,
                 )
@@ -329,36 +350,58 @@ with dpg.window(
                     width=150,
                     step=0,
                 )
-    
-    dpg.add_spacer(height=40)
 
-    with dpg.group(horizontal=False, pos=(150, 700)):
-        dpg.add_button(tag="Iniciar", label="Iniciar Medicion", callback=start_measurement)
-        dpg.add_button(tag="Salir", label="Salir", callback=exit_application)
+        dpg.add_spacer(height=20)
+
+        # Botón "Iniciar" con tema verde
+        dpg.add_button(tag="Iniciar", label="Iniciar medición", callback=start_measurement, width=310)
+        dpg.bind_item_theme("Iniciar", "green_button_theme")
+
+        dpg.add_spacer(height=20)
+
+        # Botón "Salir" con tema rojo
+        dpg.add_button(tag="Salir", label="Salir", callback=exit_application, width=310)
+        dpg.bind_item_theme("Salir", "red_button_theme")
+
 
 # -----------------------------
 #  ALERTA MODAL
 # -----------------------------
 with dpg.window(
-    label="Archivos Guardados",
+    label="Archivos guardados",
     tag="alert_window",
     modal=True,
     show=False,
     no_title_bar=True,
-    width=350,
-    height=120,
-    pos=(alert_pos_x, alert_pos_y),
+    width=250,
+    height=130,  # Ajustamos un poco la altura para mejor diseño
+    pos=(100, 300),
     no_move=True
 ):
-    dpg.add_text("", tag="alert_text")
-    dpg.add_spacer(height=10)
-    with dpg.group(horizontal=True):
-        dpg.add_button(label="Volver", callback=close_alert)
-        # Aquí definimos el botón con tag="open_folder_button"
-        dpg.add_button(label="Ir a la carpeta", 
-                       tag="open_folder_button", 
-                       callback=open_folder, 
-                       user_data="")  # El 'user_data' se actualizará en stop_measurement
+    # Crear un grupo para centrar vertical y horizontalmente
+    dpg.add_spacer(height=10)  # Más espacio entre el texto y los botones
+    with dpg.group(horizontal=False):  # Ajustamos la indentación
+        dpg.add_text("", tag="alert_text", indent=20)  # Ajustamos el texto un poco más a la izquierda
+        dpg.add_spacer(height=5)  # Más espacio entre el texto y los botones
+        
+        # Botón "Ver resultados"
+        dpg.add_button(
+            label="Ver resultados", 
+            tag="open_folder_button", 
+            callback=open_folder, 
+            user_data="",
+            indent=65
+        )
+        dpg.bind_item_theme("open_folder_button", "green_button_theme")
+
+        # Botón "Volver"
+        dpg.add_button(
+            label="Volver", 
+            tag="back", 
+            callback=close_alert,
+            indent=90
+        )
+        dpg.bind_item_theme("back", "dark_yellow_button_theme")
 
 # -----------------------------
 #  VENTANA DE MEDICIÓN
@@ -425,15 +468,16 @@ with dpg.window(
 
     dpg.add_spacer(height=30)
     dpg.add_button(tag="Detener",
-                   label="Detener Medicion",
+                   label="Detener medición",
                    callback=stop_measurement,
                    width=150,
                    pos=(screen_width - 200, screen_height - 100))
+    dpg.bind_item_theme("Detener", "red_button_theme")
 
 # -----------------------------
 #  CREAR VIEWPORT & ARRANCAR (BUCLE MANUAL)
 # -----------------------------
-dpg.create_viewport(title='Formulario de Configuracion', width=400, height=800, resizable=False, decorated=False)
+dpg.create_viewport(title='Configuración inicial', width=400, height=800, resizable=False, decorated=False)
 dpg.setup_dearpygui()
 dpg.set_viewport_pos((viewport_x, viewport_y))
 dpg.show_viewport()
